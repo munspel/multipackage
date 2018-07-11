ae1bfc8114d6e51d20377c0359204440a9290ae0
L. Andrey <munspel@ukr.net>
2018-07-11 18:45:14 +0300

    Common

---
+++ src/YourClass.php
@@ -0,0 +1,17 @@
+<?php namespace tripup\common;
+
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author yourname
+*/
+class YourClass{
+
+   public function __construct()
+   {
+       echo "It is common class";
+   }
+}
